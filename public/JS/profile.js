f161eb642bcd9bcfb96792a4a760dc31dd3acb1a
Adam MacKinnon <mackinnon.adam@gmail.com>
2020-12-25 15:59:49 -0500

    finally got database to write!  In case I mess it up.

--- public/JS/profile.js
+++ public/JS/profile.js
@@ -1,29 +1,38 @@
 
-const auth = firebase.auth();
-const whenSignedIn = document.getElementById('whenSignedIn');
-const whenSignedOut = document.getElementById('whenSignedOut');
-const signOutButton = document.getElementById('signOutButton');
-const userDetails = document.getElementById('userDetails');
-signOutButton.onclick = () => auth.signOut();
 
-const auth = firebase.auth();
-// const db = firebase.firestore();
+
+db.collection('profileData')
+// const whenSignedIn = document.getElementById('whenSignedIn');
+// const whenSignedOut = document.getElementById('whenSignedOut');
+// const signOutButton = document.getElementById('signOutButton');
+
+// signOutButton.onclick = () => auth.signOut();
+
+// const auth = firebase.auth();
+
 
 const profileForm = document.getElementById('profileForm');
 const submitButton = document.getElementById('submitButton');
 
-
-
-profileForm.addEventListener('submitButton',(e)=>{
+profileForm.addEventListener('submit', (e) =>{
     e.preventDefault();
-    db.collection('profileData').add({
-        firstName: profileForm.firstName.value,
-        lastName: profileForm.lastName.value
 
+
+    db.collection("profileData").add({
+        firstName : profileForm.firstname.value,
+        lastName : profileForm.lastname.value,
+        preference : profileForm.cityOrSuburb.value,
+        expenses : profileForm.expenses.value, 
+        income : profileForm.income.value
+
+    })
+    .then(function() {
+        console.log("profile built!");
+    })
+    .catch(function(error) {
+        console.error("Error building profile: ", error);
     });
 
 })
-
-
 
 
