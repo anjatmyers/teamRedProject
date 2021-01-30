cf4886b1d1cbdea742dc46784f680d3025508042
Adam MacKinnon <mackinnon.adam@gmail.com>
2021-01-29 20:48:25 -0500

    cleaned up firebase code, signout logic, firebase key

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,34 +1,14 @@
 const auth = firebase.auth();
-
-const whenSignedIn = document.getElementById('whenSignedIn');
-const whenSignedOut = document.getElementById('whenSignedOut');
 const signInButton = document.getElementById('signInButton');
-const signOutButton = document.getElementById('signOutButton');
-// const signOutButton1 = document.getElementById('signOutButton1');
-const userDetails = document.getElementById('userDetails');
-const signUpButton = document.getElementById('signUpButton');
-
-
 const provider = new firebase.auth.GoogleAuthProvider();
 
-
 signInButton.onclick = () => auth.signInWithPopup(provider);
-
-
-
-
 
 auth.onAuthStateChanged(user => {
     if (user) {
         window.location.href="home.html"
         // signed in... send to financials page
-        whenSignedIn.hidden = false;
-        whenSignedOut.hidden = true;
         userDetails.innerHTML = `<h2> Welcome, ${user.displayName}!</h2>`;
-        signInButton.hidden = true;
-        // signOutButton.hidden = false;
-        signUpButton.hidden = true;
-
 
     } else {
         // not signed in.  Prompt log-in.
--- public/JS/auth.js
+++
@@ -1,5 +0,0 @@
-const signOutButton = document.getElementById('signOutButton');
-const signOutButton1 = document.getElementById('signOutButton1');
-const signOutButtonAU = document.getElementById('signOutButtonAU');
-signOutButton.onclick = () => auth.signOut();
-signOutButton1.onclick = () => auth.signOut();
